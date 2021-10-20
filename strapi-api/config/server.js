e2ef7441b9166a525d1998d900194cd9b094681a
Jared Henry <jared.klayton.henry@gmail.com>
2021-10-20 19:35:01 +0000

    added strapi

---
+++ strapi-api/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'e275cf13f941e4622f68dd04398cd433'),
+    },
+  },
+});
---
+++ strapi-api/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'bcce4118-23a8-44e7-8803-205ff3c9502e'
+};
